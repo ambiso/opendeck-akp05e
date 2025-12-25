3122732461bdd120ed67b16d3e8ae263ecfeae18
ambiso <ambisotopy@gmail.com>
2025-12-25 14:15:10 +0100

    add VSDN4Pro

--- src/mappings.rs
+++ src/mappings.rs
@@ -16,7 +16,11 @@ pub const DEVICE_TYPE: u8 = 7; // StreamDeckPlus
 pub enum Kind {
     Akp05E,
     N4EN,
+    N4Pro,
 }
+
+pub const VSDINSIDE_VID: u16 = 0x5548;
+pub const N4_PRO_PID: u16 = 0x1023;
 
 pub const AJAZZ_VID: u16 = 0x0300;
 pub const AKP05E_PID: u16 = 0x3004;
@@ -27,8 +31,9 @@ pub const N4EN_PID: u16 = 0x1007;
 // Map all queries to usage page 65440 and usage id 1 for now
 pub const AKP05E_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, AJAZZ_VID, AKP05E_PID);
 pub const N4EN_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, MIRABOX_VID, N4EN_PID);
+pub const N4_PRO_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, VSDINSIDE_VID, N4_PRO_PID);
 
-pub const QUERIES: &[DeviceQuery] = &[AKP05E_QUERY, N4EN_QUERY];
+pub const QUERIES: &[DeviceQuery] = &[AKP05E_QUERY, N4EN_QUERY, N4_PRO_QUERY];
 
 impl Kind {
     /// Matches devices VID+PID pairs to correct kinds
@@ -44,6 +49,11 @@ impl Kind {
                 _ => None,
             },
 
+            VSDINSIDE_VID => match pid {
+                N4_PRO_PID => Some(Kind::N4Pro),
+                _ => None,
+            },
+
             _ => None,
         }
     }
@@ -54,6 +64,7 @@ impl Kind {
         match &self {
             Self::Akp05E => "Ajazz AKP05E",
             Self::N4EN => "Mirabox N4EN",
+            Self::N4Pro => "VSDInside N4 Pro",
         }
         .to_string()
     }
@@ -63,6 +74,7 @@ impl Kind {
         match self {
             Self::N4EN => 3,
             Self::Akp05E => 3,
+            Self::N4Pro => 3,
         }
     }
 
